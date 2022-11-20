328de7027d9a4667115ed2ea7c40aa8c9b29696f
Hermawan Arby <hermawanarby2@gmail.com>
2022-11-20 19:24:02 +0700

    Menampilkan pdf

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -4,15 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Certificate;
 use Illuminate\Http\Request;
+use PDF;
 
 class CertificateController extends Controller
 {
     public function search(Request $request)
     {
+        
         if ($request->search == null) {
             return view('/sertifikat');
         }
         $certificate = Certificate::all()->where('sertifikat_id','=',($request->search));
-        return view('sertifikat',['sertifikat'=>$certificate]);
+        // return view('sertifikat',['sertifikat'=>$certificate]);
+        
+        $pdf = PDF::loadHTML($certificate)
+        ->setPaper('a4', 'landscape');
+        
+        return $pdf->stream();
+        
     }
 }
